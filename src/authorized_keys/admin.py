2ae5e765cdb562040b875025ae722e63352ee214
NatLee <natlee.work@gmail.com>
2024-02-22 00:39:36 +0800

    [update] add service key on admin page

--- src/authorized_keys/admin.py
+++ src/authorized_keys/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from authorized_keys.models import ReverseServerAuthorizedKeys
 from authorized_keys.models import UserAuthorizedKeys
-
+from authorized_keys.models import ServiceAuthorizedKeys
 @admin.register(ReverseServerAuthorizedKeys)
 class ReverseServerAuthorizedKeysAdmin(admin.ModelAdmin):
-    list_display = ('hostname', 'display_key', 'created_at', 'updated_at')
-    search_fields = ('hostname', 'key', 'description')
+    list_display = ('hostname', 'display_key', 'reverse_port', 'created_at', 'updated_at')
+    search_fields = ('hostname', 'key', 'reverse_port', 'description')
 
     def display_key(self, obj):
         key = obj.key
@@ -18,6 +18,18 @@ class ReverseServerAuthorizedKeysAdmin(admin.ModelAdmin):
 class UserAuthorizedKeysAdmin(admin.ModelAdmin):
     list_display = ('hostname', 'display_key', 'created_at', 'updated_at')
     search_fields = ('hostname', 'key', 'description')
+
+    def display_key(self, obj):
+        key = obj.key
+        if len(key) > 10:
+            return f"{key[:20]} ... {key[-20:]}"
+        return key
+    display_key.short_description = 'Key'
+
+@admin.register(ServiceAuthorizedKeys)
+class ServiceAuthorizedKeysAdmin(admin.ModelAdmin):
+    list_display = ('service', 'display_key', 'created_at', 'updated_at')
+    search_fields = ('service', 'key', 'description')
 
     def display_key(self, obj):
         key = obj.key
