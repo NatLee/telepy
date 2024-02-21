1f71280f6019b266d05bc1c3c39535461ddb1350
Nat Lee <natlee.work@gmail.com>
2024-02-21 17:51:20 +0800

    [update] add user auth keys model

--- src/authorized_keys/admin.py
+++ src/authorized_keys/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
-from authorized_keys.models import AuthorizedKeys
+from authorized_keys.models import ReverseServerAuthorizedKeys
+from authorized_keys.models import UserAuthorizedKeys
 
-@admin.register(AuthorizedKeys)
-class AuthorizedKeysAdmin(admin.ModelAdmin):
+@admin.register(ReverseServerAuthorizedKeys)
+class ReverseServerAuthorizedKeysAdmin(admin.ModelAdmin):
+    list_display = ('hostname', 'display_key', 'created_at', 'updated_at')
+    search_fields = ('hostname', 'key', 'description')
+
+    def display_key(self, obj):
+        key = obj.key
+        if len(key) > 10:
+            return f"{key[:20]} ... {key[-20:]}"
+        return key
+    display_key.short_description = 'Key'
+
+@admin.register(UserAuthorizedKeys)
+class UserAuthorizedKeysAdmin(admin.ModelAdmin):
     list_display = ('hostname', 'display_key', 'created_at', 'updated_at')
     search_fields = ('hostname', 'key', 'description')
 
--- src/authorized_keys/models.py
+++ src/authorized_keys/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-class AuthorizedKeys(models.Model):
+class ReverseServerAuthorizedKeys(models.Model):
+    # Reverse Server Authorized Keys for endpoint to connect with this SSH server
 
     hostname = models.CharField(max_length=128, unique=True)
     key = models.CharField(max_length=19200, unique=True, blank=False, null=False) # SSH Key (public)
@@ -10,10 +11,27 @@ class AuthorizedKeys(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f'[{self.hostname}] {self.key[:20]} ... {self.key[-20:]}'
+
+    class Meta:
+        verbose_name = 'Reverse Server Authorized Key'
+        verbose_name_plural = 'Reverse Server Authorized Keys'
+
+class UserAuthorizedKeys(models.Model):
+    # User Authorized Keys for reversed SSH tunnel
+
+    hostname = models.CharField(max_length=128, unique=True)
+    key = models.CharField(max_length=19200, unique=True, blank=False, null=False) # SSH Key (public)
+
+    description = models.TextField(blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'[{self.hostname}] {self.key[:20]} ... {self.key[-20:]}'
 
     class Meta:
-        verbose_name = 'Authorized Key'
-        verbose_name_plural = 'Authorized Keys'
+        verbose_name = 'User Authorized Key'
+        verbose_name_plural = 'User Authorized Keys'
