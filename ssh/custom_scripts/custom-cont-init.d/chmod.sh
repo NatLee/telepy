b64a3f19675b9c6c35026b803d83780927f9704b
Nat Lee <natlee.work@gmail.com>
2024-02-21 23:13:59 +0800

    [update] SSH custom scripts

--- ssh/custom_scripts/custom-cont-init.d/chmod.sh
+++ ssh/custom_scripts/custom-cont-init.d/chmod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "-------- Change mode with keys"
+echo "-------- Change mode with keys --------"
 chmod 400 /config/ssh_host_keys/*_key
 chmod 644 /config/ssh_host_keys/*_key.pub
-
+echo "-------- Change mode with custom-cont-init.d --------"
 chmod 755 /custom-cont-init.d/*
