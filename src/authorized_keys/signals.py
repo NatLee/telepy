c036f08f893705fe02b8747d3c31dbac6e51fcec
NatLee <natlee.work@gmail.com>
2024-02-21 17:51:20 +0800

    [update] add user auth keys model

--- src/authorized_keys/signals.py
+++ src/authorized_keys/signals.py
@@ -1,12 +1,12 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from authorized_keys.models import AuthorizedKeys
+from authorized_keys.models import ReverseServerAuthorizedKeys
 import os
 
-@receiver(post_save, sender=AuthorizedKeys)
-@receiver(post_delete, sender=AuthorizedKeys)
+@receiver(post_save, sender=ReverseServerAuthorizedKeys)
+@receiver(post_delete, sender=ReverseServerAuthorizedKeys)
 def update_authorized_keys(sender, **kwargs):
-    keys = AuthorizedKeys.objects.all()
+    keys = ReverseServerAuthorizedKeys.objects.all()
     authorized_keys_content = "\n".join([key.key for key in keys])
     with open(os.path.expanduser('/ssh/authorized_keys'), 'w') as f:
         f.write(authorized_keys_content)
