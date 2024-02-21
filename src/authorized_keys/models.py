815a9b5cfe1bf74b42fcd535a8750208f65b636a
NatLee <natlee.work@gmail.com>
2024-02-21 14:32:12 +0800

    [update] change name to hostname

--- src/authorized_keys/models.py
+++ src/authorized_keys/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class AuthorizedKeys(models.Model):
 
-    username = models.CharField(max_length=100, unique=True)
+    hostname = models.CharField(max_length=100, unique=True)
     key = models.TextField()
 
     description = models.TextField(blank=True, null=True)
@@ -12,7 +12,7 @@ class AuthorizedKeys(models.Model):
 
 
     def __str__(self):
-        return f'[{self.username}] {self.key[:5]}...{self.key[-5:]}'
+        return f'[{self.hostname}] {self.key[:5]}...{self.key[-5:]}'
 
     class Meta:
         verbose_name = 'Authorized Key'
