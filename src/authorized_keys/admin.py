69d35f8ed1c495af173a129018841f3d34f0e297
Nat Lee <natlee.work@gmail.com>
2024-02-21 14:32:12 +0800

    [update] change name to hostname

--- src/authorized_keys/admin.py
+++ src/authorized_keys/admin.py
@@ -3,6 +3,6 @@ from authorized_keys.models import AuthorizedKeys
 
 @admin.register(AuthorizedKeys)
 class AuthorizedKeysAdmin(admin.ModelAdmin):
-    list_display = ('username', 'key', 'created_at', 'updated_at')
-    search_fields = ('username', 'description')
+    list_display = ('hostname', 'key', 'created_at', 'updated_at')
+    search_fields = ('hostname', 'description')
 
