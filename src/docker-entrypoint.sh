784932530c4772467ba6313e9644146b1635ce79
NatLee <natlee.work@gmail.com>
2024-02-22 00:37:02 +0800

    [update] add SSH config file permissions

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+# Set the correct permissions for the SSH config file
+chown -R root:root /root/.ssh/config
+
+# Migrate the database
 python manage.py makemigrations && python manage.py migrate
+
+# Start services
 /usr/local/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
