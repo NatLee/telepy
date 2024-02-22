243fb340774094bdbbb876bcb2d90a7b2c895b9d
NatLee <natlee.work@gmail.com>
2024-02-22 11:34:36 +0800

    [fix] path of app

--- dev-startapp.sh
+++ dev-startapp.sh
@@ -3,9 +3,9 @@ export $(cat ./.env)
 docker exec -it telepy-web bash -c "python manage.py startapp `echo $@`"
 
 # check the folder is created
-if [ ! -d "./backend/$@" ]; then
+if [ ! -d "./src/$@" ]; then
     echo "Folder not created"
     exit 1
 fi
 # if created, we need to make it editable
-sudo chown -R $USER:$USER ./backend/$@
+sudo chown -R $USER:$USER ./src/$@
