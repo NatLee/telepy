32bc94dac107118812cd7f548880c911c8353008
Nat Lee <natlee.work@gmail.com>
2024-02-22 15:08:21 +0800

    [update] add permission class

--- src/authorized_keys/views.py
+++ src/authorized_keys/views.py
@@ -1,11 +1,15 @@
 from rest_framework import generics
-from .models import ReverseServerAuthorizedKeys
-from .serializers import ReverseServerAuthorizedKeysSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
+
 from drf_yasg.utils import swagger_auto_schema
+
+from authorized_keys.models import ReverseServerAuthorizedKeys
+from authorized_keys.serializers import ReverseServerAuthorizedKeysSerializer
 
 class ReverseServerAuthorizedKeysList(generics.ListAPIView):
     queryset = ReverseServerAuthorizedKeys.objects.all()
     serializer_class = ReverseServerAuthorizedKeysSerializer
+    permission_classes = (IsAuthenticated,)
 
     @swagger_auto_schema(tags=['Reverse Server Keys'])
     def get(self, request, *args, **kwargs):
