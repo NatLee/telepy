8c59ad64c71d0f8044ce29efff21c84c80814edb
Nat Lee <natlee.work@gmail.com>
2024-02-23 21:56:20 +0800

    [update] CRUD of user keys

--- src/authorized_keys/serializers.py
+++ src/authorized_keys/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from authorized_keys.models import ReverseServerAuthorizedKeys
+from authorized_keys.models import UserAuthorizedKeys
 
 class ReverseServerAuthorizedKeysSerializer(serializers.ModelSerializer):
     class Meta:
         model = ReverseServerAuthorizedKeys
-        fields = ['hostname', 'key', 'reverse_port', 'description', 'created_at', 'updated_at']
+        fields = ['id', 'hostname', 'key', 'reverse_port', 'description', 'created_at', 'updated_at']
+
+class UserAuthorizedKeysSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserAuthorizedKeys
+        fields = ['id', 'hostname', 'key', 'description', 'created_at', 'updated_at']
