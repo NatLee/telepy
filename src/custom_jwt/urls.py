f7b038ad79813f9617481bb26a0dc65965d2c470
NatLee <natlee.work@gmail.com>
2024-02-23 13:53:18 +0800

    [update] add tags to API

--- src/custom_jwt/urls.py
+++ src/custom_jwt/urls.py
@@ -1,14 +1,10 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from rest_framework_simplejwt.views import (
-    TokenVerifyView,
-)
-
 from custom_jwt import views
 
 urlpatterns = [
-    path("token", views.MyTokenObtainPairView.as_view(), name="token_get"),
-    path("token/refresh", views.MyTokenRefreshView.as_view(), name="token_refresh"),
-    path("token/verify", TokenVerifyView.as_view(), name="token_verify"),
+    path("token", views.MyTokenObtainPairView.as_view(), name="token-get"),
+    path("token/refresh", views.MyTokenRefreshView.as_view(), name="token-refresh"),
+    path("token/verify", views.MyTokenVerifyView.as_view(), name="token-verify"),
 ]
