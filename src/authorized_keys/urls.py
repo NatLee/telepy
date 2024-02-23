aceb5c54599947b6ae1b9581d2b1ad3bcc3b4116
NatLee <natlee.work@gmail.com>
2024-02-23 21:56:20 +0800

    [update] CRUD of user keys

--- src/authorized_keys/urls.py
+++ src/authorized_keys/urls.py
@@ -1,7 +1,20 @@
-from django.urls import path
+from django.urls import path, include
 from authorized_keys.views import ReverseServerAuthorizedKeysList
 from authorized_keys.views import CheckReverseServerPortStatus
+
 urlpatterns = [
+
     path('server/keys', ReverseServerAuthorizedKeysList.as_view(), name='reverse-server-keys-list'),
     path('server/status/ports', CheckReverseServerPortStatus.as_view(), name='reverse-server-ports-status'),
+
+]
+
+from rest_framework.routers import DefaultRouter
+from authorized_keys.views import UserAuthorizedKeysViewSet
+
+router = DefaultRouter(trailing_slash=False)
+router.register(r'user/keys', UserAuthorizedKeysViewSet)
+
+urlpatterns += [
+    path('', include(router.urls)),
 ]
--- src/authorized_keys/views.py
+++ src/authorized_keys/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
@@ -7,6 +8,8 @@ from drf_yasg.utils import swagger_auto_schema
 
 from authorized_keys.models import ReverseServerAuthorizedKeys
 from authorized_keys.serializers import ReverseServerAuthorizedKeysSerializer
+from authorized_keys.models import UserAuthorizedKeys
+from authorized_keys.serializers import UserAuthorizedKeysSerializer
 
 from authorized_keys.utils import monitor_used_ports
 
@@ -32,3 +35,37 @@ class CheckReverseServerPortStatus(APIView):
         }
         return Response(result)
 
+class UserAuthorizedKeysViewSet(viewsets.ModelViewSet):
+    queryset = UserAuthorizedKeys.objects.all()
+    serializer_class = UserAuthorizedKeysSerializer
+    permission_classes = [IsAuthenticated]
+
+    @swagger_auto_schema(tags=['Reverse Server Keys'])
+    def list(self, request, *args, **kwargs):
+        """List all user authorized keys"""
+        return super().list(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=['Reverse Server Keys'])
+    def create(self, request, *args, **kwargs):
+        """Create a new user authorized key"""
+        return super().create(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=['Reverse Server Keys'])
+    def retrieve(self, request, *args, **kwargs):
+        """Retrieve a specific user authorized key"""
+        return super().retrieve(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=['Reverse Server Keys'])
+    def update(self, request, *args, **kwargs):
+        """Update a user authorized key"""
+        return super().update(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=['Reverse Server Keys'])
+    def partial_update(self, request, *args, **kwargs):
+        """Partial update a user authorized key"""
+        return super().partial_update(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=['Reverse Server Keys'])
+    def destroy(self, request, *args, **kwargs):
+        """Delete a user authorized key"""
+        return super().destroy(request, *args, **kwargs)
