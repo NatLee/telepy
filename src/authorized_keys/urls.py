ae817efe1af468516ff81979a841aaa6c2d99d27
Nat Lee <natlee.work@gmail.com>
2024-02-25 18:34:41 +0800

    [update] add service keys API

--- src/authorized_keys/urls.py
+++ src/authorized_keys/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from authorized_keys.views import CheckReverseServerPortStatus
 from authorized_keys.views import ReverseServerUsernamesMapServerId
+from authorized_keys.views import ServiceAuthorizedKeysListView
 urlpatterns = [
     path('server/status/ports', CheckReverseServerPortStatus.as_view(), name='reverse-server-ports-status'),
     path('server/<int:server_id>/usernames', ReverseServerUsernamesMapServerId.as_view(), name='reverse-server-usernames'),
+    path('service/keys', ServiceAuthorizedKeysListView.as_view(), name='service-authorized-keys'),
 ]
 
 from rest_framework.routers import DefaultRouter
