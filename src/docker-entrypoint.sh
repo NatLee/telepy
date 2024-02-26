f400c86e18c97b53eef5cfcbe4ff8ac7282145b2
Nat Lee <natlee.work@gmail.com>
2024-02-26 11:30:13 +0800

    [fix] set 600 with key

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -3,6 +3,9 @@
 # Set the correct permissions for the SSH config file
 chown -R root:root /root/.ssh/config
 
+# Set the correct permissions for the SSH key
+chmod 600 /root/.ssh/id_rsa
+
 # Migrate the database
 python manage.py makemigrations && python manage.py migrate
 
