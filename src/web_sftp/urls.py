3999c841e3721a3e4c82f43b9950603c5ce0216b
Nat Lee <natlee.work@gmail.com>
2024-03-21 14:53:30 +0800

    [update] default path in search bar & update ws when closing

--- src/web_sftp/urls.py
+++ src/web_sftp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from web_sftp.views import ShellDetect
 from web_sftp.views import ListPath
 from web_sftp.views import Download
 from web_sftp.views import UploadFiles
-
 urlpatterns = [
 
+    path('shell/<int:server_id>/<str:username>', ShellDetect.as_view(), name='sftp-shell'),
     path('list/<int:server_id>/<str:username>', ListPath.as_view(), name='list-path'),
     path('download/<int:server_id>/<str:username>', Download.as_view(), name='sftp-download'),
     path('upload/<int:server_id>/<str:username>', UploadFiles.as_view(), name='sftp-upload'),
