f63f628bfce380bacd8be8e75d2b2c0cc9aaa0cc
Nat Lee <natlee.work@gmail.com>
2024-03-26 22:42:45 +0800

    [update] add admin user signal

--- src/user_management/apps.py
+++ src/user_management/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class UserManagementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user_management'
+
+    def ready(self):
+        import user_management.signals
