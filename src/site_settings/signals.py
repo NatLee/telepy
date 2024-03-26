e447d42fb75014dfa6961900dc76b9f4741d92ec
Nat Lee <natlee.work@gmail.com>
2024-03-27 01:42:13 +0800

    [update] add signals to check first record added

---
+++ src/site_settings/signals.py
@@ -0,0 +1,7 @@
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
+from site_settings.models import SiteSettings
+
+@receiver(post_migrate)
+def create_default_site_settings(sender, **kwargs):
+    SiteSettings.get_solo()  # This will create the default settings if it doesn't exist
