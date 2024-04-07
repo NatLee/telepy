e5529160faf5cf19696405cd97bdf75f02804a92
NatLee <natlee.work@gmail.com>
2024-04-07 22:55:59 +0800

    [update] add route for Admin settings

--- src/tunnels/urls.py
+++ src/tunnels/urls.py
@@ -3,6 +3,7 @@ from tunnels.views import TunnelsIndex
 from tunnels.views import TunnelsCreate
 from tunnels.views import UserKeys
 from tunnels.views import Terminal
+from tunnels.views import AdminSettings
 from tunnels.views import SSHServerLogs
 from tunnels.views import ReverseServerAuthorizedKeysConfig
 from tunnels.views import AutoSSHTunnelScript
@@ -13,7 +14,9 @@ urlpatterns = [
     path('create', TunnelsCreate.as_view(), name='tunnels-create'),
     path('keys', UserKeys.as_view(), name='user-keys'),
     path('terminal/<int:server_id>', Terminal.as_view(), name='terminal'),
+    path('settings', AdminSettings.as_view(), name='admin-settings'),
     path('logs', SSHServerLogs.as_view(), name='ssh-server-logs'),
+
     path('server/config/<int:server_id>', ReverseServerAuthorizedKeysConfig.as_view(), name='authorized-keys-config'),
     path('server/script/autossh/<int:server_id>/<int:ssh_port>', AutoSSHTunnelScript.as_view(), name='auto-ssh-tunnel-script'),
     path('server/script/windows/<int:server_id>/<int:ssh_port>', WindowsSSHTunnelScript.as_view(), name='windows-ssh-tunnel-script'),
