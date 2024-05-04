acdfc24d57c1061feecd3f0e7f95260343b0ff69
NatLee <natlee.work@gmail.com>
2024-05-05 02:05:23 +0800

    [fix] rm dummy slash

--- src/tunnels/tunnel_script_renderer/templates/autossh_template.sh
+++ src/tunnels/tunnel_script_renderer/templates/autossh_template.sh
@@ -2,12 +2,12 @@
 
 echo "[+] Script start"
 
-autossh \\
--M 6769 \\
--o "ServerAliveInterval 30" \\
--o "ServerAliveCountMax 3" \\
--o "StrictHostKeyChecking=no" \\
--o "UserKnownHostsFile=/dev/null" \\
--p ${reverse_server_ssh_port} \\
--NR '*:${reverse_port}:localhost:${ssh_port}' \\
+autossh \
+-M 6769 \
+-o "ServerAliveInterval 30" \
+-o "ServerAliveCountMax 3" \
+-o "StrictHostKeyChecking=no" \
+-o "UserKnownHostsFile=/dev/null" \
+-p ${reverse_server_ssh_port} \
+-NR '*:${reverse_port}:localhost:${ssh_port}' \
 telepy@${server_domain}
