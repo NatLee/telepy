9f47be32314913c0f8a4cd0637b7c8af05c4a761
Nat Lee <natlee.work@gmail.com>
2024-05-07 04:13:39 +0800

    [fix] add export `$(cat ./.env)`

--- dev-create-superuser.sh
+++ dev-create-superuser.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+export $(cat ./.env)
 docker exec -it telepy-web-${PROJECT_NAME} bash -c 'DJANGO_SUPERUSER_PASSWORD=1234 python manage.py createsuperuser --noinput --username admin --email admin@admin.com'
--- dev-supervisorctl.sh
+++ dev-supervisorctl.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+export $(cat ./.env)
 docker exec -it telepy-web-${PROJECT_NAME} supervisorctl -c /etc/supervisor/conf.d/supervisord.conf
