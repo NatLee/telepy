5ec5b9dc663335700b6aff806567100928bd7394
Nat Lee <natlee.work@gmail.com>
2024-05-07 04:02:34 +0800

    [update] add `PROJECT_NAME` & update names

--- dev-backend-debug.sh
+++ dev-backend-debug.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export $(cat ./.env)
-docker exec -it telepy-web bash -c "supervisorctl -c /etc/supervisor/conf.d/supervisord.conf stop django"
-docker exec -it telepy-web bash -c "python manage.py runserver 0.0.0.0:8000"
+docker exec -it telepy-web-${PROJECT_NAME} bash -c "supervisorctl -c /etc/supervisor/conf.d/supervisord.conf stop django"
+docker exec -it telepy-web-${PROJECT_NAME} bash -c "python manage.py runserver 0.0.0.0:8000"
