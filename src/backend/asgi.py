d5cd9d551ee75687e4b667557c93d523ff68bf70
NatLee <natlee.work@gmail.com>
2024-06-05 15:49:23 +0800

    [update] Django to `5.0.6`

--- src/backend/asgi.py
+++ src/backend/asgi.py
@@ -1,10 +1,6 @@
 """
 ASGI config for backend project.
-
 It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
--- src/backend/urls.py
+++ src/backend/urls.py
@@ -1,8 +1,7 @@
 """
 URL configuration for backend project.
+The `urlpatterns` list routes URLs to views.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
