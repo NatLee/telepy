8c2e5ac77d55fd6ccaa5ec430a4fb9208e0aac5e
Nat Lee <natlee.work@gmail.com>
2026-02-22 03:49:19 +0800

    fix: update permissions and enhance installation script
    - Changed file permissions for chmod.sh and install.sh to make them executable.
    - Updated install.sh to include installation of net-tools alongside iproute2, improving network utility availability.

--- ssh/custom_scripts/custom-cont-init.d/install.sh
+++ ssh/custom_scripts/custom-cont-init.d/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-echo "-------- Install iproute2 --------"
-apk add --no-cache iproute2
+echo "-------- Install iproute2 (ss) --------"
+apk add --no-cache iproute2 || true
+echo "-------- Install net-tools (netstat) --------"
+apk add --no-cache net-tools || true
 echo "-------- Install redis --------"
 apk add --no-cache redis
 echo "-------- Install curl --------"
