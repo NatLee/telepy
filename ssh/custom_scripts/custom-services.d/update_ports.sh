9f928adf70f20006f465b9ee31c4250f658bb61e
Nat Lee <natlee.work@gmail.com>
2026-02-22 02:07:31 +0800

    fix: improve error handling in update_ports.sh script
    - Updated the command to retrieve listening sockets, using ss or netstat as a fallback.
    - Redirected error output to /dev/null to prevent script interruptions from command failures.

--- ssh/custom_scripts/custom-services.d/update_ports.sh
+++ ssh/custom_scripts/custom-services.d/update_ports.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-  echo `ss -tlnp` | redis-cli -h redis -x SET ss_output > /dev/null
+  echo $(/usr/bin/ss -tlnp 2>/dev/null || /bin/netstat -tlnp 2>/dev/null) | /usr/bin/redis-cli -h redis -x SET ss_output > /dev/null 2>&1
   sleep 3
 done
 
